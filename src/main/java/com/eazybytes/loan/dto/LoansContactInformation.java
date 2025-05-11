90b8674b57b4fcfad2c245f8c3957f1a0fa9b894
Bharat Hegde <bharathegde@Bharats-MacBook-Air.local>
2025-05-11 11:52:03 +0530

    update config to use spring cloud

--- src/main/java/com/eazybytes/loan/dto/LoansContactInformation.java
+++ src/main/java/com/eazybytes/loan/dto/LoansContactInformation.java
@@ -1,14 +1,17 @@
 package com.eazybytes.loan.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 import java.util.Map;
 
 @ConfigurationProperties(prefix = "loan")
-public record LoansContactInformation(
-        String message,
-        Map<String, String> contactDetails,
-        List<String> onCallSupport
-) {
+@Getter
+@Setter
+public class LoansContactInformation{
+    String message;
+    Map<String, String> contactDetails;
+    List<String> onCallSupport;
 }
